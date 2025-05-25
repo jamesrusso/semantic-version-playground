f7309d93e63e1f95696c356fc48da726b6fd96d4
James Russo <jr@halo3.net>
2025-05-25 13:48:08 -0400

    Feat/working on some feature (#2)
    * Add New Feature (#1)
    
    * build: add next channel
    
    * feat: add new feature on next
    
    * build: add next to github actions
    
    * chore(release): 1.1.0-next.1 [skip ci]
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    
    * feat: here is another feature
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import { VERSION } from './version.js';
 console.log(`This is ${VERSION} of the app`);
 
 console.log("This a new feature!")
+console.log("This is another new feature via the feature branch!")
